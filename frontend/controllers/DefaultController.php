a759807947952eee494a8d5fed608d1813059cf1
Thom P <tpalstra@gmail.com>
2017-09-29 17:21:23 +0200

    TINGS

--- frontend/controllers/DefaultController.php
+++ frontend/controllers/DefaultController.php
@@ -16,5 +16,28 @@ class DefaultController extends Controller{
             'exception' => \IO::$app->exception,
         ]);
     }
+
+    public function actionDownloadMap(){
+
+        //frontend/web/img/56/100x100.png
+        $id = $_GET['id'];
+
+        $path = \IO::$app->root . DIRECTORY_SEPARATOR . "frontend/web/img/$id/100x100.png";
+
+        if(file_exists($path)){
+            $size = filesize($path);
+            header('Content-Description: File Transfer');
+            header('Content-Type: image/png');
+            header('Content-Disposition: attachment; filename="Image.png"');
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . $size);
+            readfile($path);
+        }
+
+
+    }
 }
 ?>
