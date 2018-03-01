b47e0b44ef5a0f9813ad47bb4fbe445f5716e93c
Thom Palstra <tpalstra@gmail.com>
2018-03-01 19:56:55 +0100

    UNIX compat

--- scope/autoload.php
+++ scope/autoload.php
@@ -1,5 +1,6 @@
 <?php
 spl_autoload_register(function ($class) {
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
     if( file_exists( dirname(__DIR__) . DIRECTORY_SEPARATOR . $class . '.php') ){
         include( dirname(__DIR__) . DIRECTORY_SEPARATOR . $class . '.php' );
     }
